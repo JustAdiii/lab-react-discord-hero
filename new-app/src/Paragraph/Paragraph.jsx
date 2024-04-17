0075949301a93e0b6ff38640e82cd81ee95bb884
JustAdiii <adriantloczek@gmail.com>
2024-04-17 14:54:09 +0200

    Done

---
+++ new-app/src/Paragraph/Paragraph.jsx
@@ -0,0 +1,14 @@
+import "./Paragraph.css";
+
+const Paragraph = () => {
+  return (
+    <p id="paragraph">
+      ...where you can belong to a school club, a gaming group, or a worldwide
+      art community. Where just you and a handful of friends can spend time
+      together. A place that makes it easy to talk every day and hang out more
+      often.
+    </p>
+  );
+};
+
+export default Paragraph;
